7f0cfc06b9c2cb7cdbffc528aaad961d56c2e3dd
Hiroko Nakano <hirokonakano@hirokonomacbook-pro.local>
2018-11-05 21:56:01 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Hiroko Nakano on 2018/11/05.
+//  Copyright © 2018 katenakano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
